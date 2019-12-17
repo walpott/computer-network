70c328968b1dca1ba48e4d7a6d4690f5e6a9aaab
walpott <walpott@outlookcom>
2019-12-17 10:58:11 +0800

    router.h

---
+++ router.h
@@ -0,0 +1,35 @@
+#ifndef ROUTER_H_
+#define ROUTER_H_
+
+#include "picture.h"
+
+typedef struct router
+{
+	int address[4];
+	int next[4];
+	router()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			address[i] = 0;
+			next[i] = 0;
+		}
+	}
+}router;
+
+
+class Router
+{
+public:
+	Router();
+	void Dijkstra(Graph& G, char ch);
+	void tablecreate(Graph& G, char ch);
+	void show(int v);
+private:
+	int tablesize;
+	int* path;
+	int* dist;
+	router* table;
+};
+
+#endif
